a3cec98cf14e095bc57e4a52a6be5e690fe12f35
Jefferson Araújo <jeffersonc201212@gmail.com>
2022-10-19 21:49:17 -0300

    Mensagem de fim de jogo implementada e código refatorado

---
+++ js/gamePlay.js
@@ -0,0 +1,58 @@
+import {numeroDeCartas} from "./novoJogo.js";
+
+let numViradas = 0,
+  cartasViradas = [],
+  cartasViradasRodada = [];
+
+function virarCarta(carta) {
+  carta.classList.add("carta--virada");
+  cartasViradas.push(carta);
+  cartasViradasRodada.push(carta);
+  numViradas++;
+  console.log(numViradas);
+}
+
+function desvirarCartasRodada() {
+  cartasViradasRodada.forEach((cartaVirada) => {
+    cartaVirada.classList.remove("carta--virada");
+    removerItemDeVetor(cartaVirada, cartasViradas);
+  });
+  cartasViradasRodada = [];
+}
+
+function removerItemDeVetor(item, vetor) {
+  const indice = vetor.indexOf(item);
+  vetor.splice(indice, 1);
+}
+
+function controladorClickNaCarta(carta) {
+  //Verifica se a carta clicada está virada
+  let ehVirada = carta.classList.contains("carta--virada");
+
+  /*Se a quantidade de carta virada for menor que 2 e a carta 
+    clicada não tiver sido virada: vira-se a carta*/
+  if (cartasViradasRodada.length < 2 && !ehVirada) {
+    virarCarta(carta);
+  }
+
+  //Quando o jogador virar duas cartas...
+  if (cartasViradasRodada.length === 2) {
+    //...verifica-se se elas são iguais.
+    if (cartasViradasRodada[0].isEqualNode(cartasViradasRodada[1])) {
+      //Se forem, apenas zera-se a lista de cartasViradasRodada
+      cartasViradasRodada = [];
+      if (cartasViradas.length == numeroDeCartas) finalizarJogo();
+    } else {
+      //Senão após 1000ms = 1s as cartas da lista são desviradas
+      setTimeout(() => {
+        desvirarCartasRodada();
+      }, 1000);
+    }
+  }
+}
+
+function finalizarJogo() {
+  alert(`Você ganhou em ${numViradas} jogadas!`);
+}
+
+export default controladorClickNaCarta;
---
+++ js/novoJogo.js
@@ -0,0 +1,61 @@
+import controladorClickNaCarta from "./gamePlay.js";
+
+let numeroDeCartas;
+
+function getCartas() {
+  const cartas = [];
+
+  for (let i = 0; i < numeroDeCartas / 2; i++) {
+    const novaCarta = document.createElement("div");
+    novaCarta.classList.add("carta", `carta--${i}`);
+    novaCarta.innerHTML = /*html*/ `
+      <div class="face frente"></div>
+      <div class="face verso"></div>
+    `;
+    cartas.push(novaCarta.cloneNode(true));
+    cartas.push(novaCarta.cloneNode(true));
+  }
+
+  for (let carta in cartas){
+    cartas[carta].onclick = (e) => controladorClickNaCarta(e.currentTarget);
+  }
+
+  return cartas;
+}
+
+function embaralharCartas(cartas) {
+  //Algoritmo Fisher-Yates
+  for (let i = cartas.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const aux = cartas[j];
+    cartas[j] = cartas[i];
+    cartas[i] = aux;
+  }
+  return cartas;
+}
+
+function distribuirCartas(cartas) {
+  const appArea = document.querySelector(".app");
+  appArea.innerHTML = "";
+
+  cartas.forEach((carta) => {
+    appArea.appendChild(carta);
+  });
+}
+
+function novoJogo() {
+  do {
+    numeroDeCartas = prompt("Com quantas cartas quer jogar?");
+  } while (
+    numeroDeCartas < 4 ||
+    numeroDeCartas > 14 ||
+    numeroDeCartas % 2 !== 0
+  );
+
+  let cartas = getCartas();
+  cartas = embaralharCartas(cartas);
+  distribuirCartas(cartas);
+}
+
+export default novoJogo;
+export { numeroDeCartas };
